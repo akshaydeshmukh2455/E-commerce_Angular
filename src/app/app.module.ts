0724473edcf2b51a926e127b7fc23a0b8434e4f4
akshaydeshmukh2455 <akshdeshmukh123@gmail.com>
2023-06-27 18:10:34 +0530

    created seller signup form routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SellerAuthComponent
+    SellerAuthComponent,
+    SellerHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { SellerService } from '../services/seller.service';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit{
-  constructor(){}
+  constructor(private seller:SellerService,private router:Router){}
   ngOnInit(){
     console.log("Seller Auth");
   }
-  signUp(data:object){
-    console.warn(data)
+  signUp(data:object):void{
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+      console.warn(result)
+    });
   }
 
 }
